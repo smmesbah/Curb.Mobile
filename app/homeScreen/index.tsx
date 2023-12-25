7b1169c1ba546b308b1c45179bc5aeccb995be2e
smmesbah <mesbah@recruit.so>
2023-12-25 18:31:20 +0600

    Add Today's Task API calls

--- app/homeScreen/index.tsx
+++ app/homeScreen/index.tsx
@@ -14,30 +14,33 @@ import MyProfile from 'app/myProfileScreen/MyProfile';
 import { useNavigation } from '@react-navigation/native';
 import { router } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
-const windowWidth=Dimensions.get('window').width;
-const windowHeight=Dimensions.get('window').height;
-const dummyValues=[
-    {
-        title: 'Play Basketball',
-        Description: 'Play basketball with friends',
-        imageUri: require('../../assets/images/BasketBall.webp'),
-    }
+interface taskMetaData {
+  id: number,
+  attributes: {
+    taskTitle: string,
+    taskType: string,
+    taskDescription: string,
+  }
+}
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const dummyValues = [
+  {
+    title: 'Play Basketball',
+    Description: 'Play basketball with friends',
+    imageUri: require('../../assets/images/BasketBall.webp'),
+  }
 ]
 
 
 const Home = () => {
 
-    const [keyboardOpen, setKeyboardOpen] = useState(false);
-    const [calandermodalOpen, setCalanderModalOpen] = useState(false);
-    // const handleSecret = async() => {
-    //   const value=await AsyncStorage.getItem('token');
-    //   console.log(value)
-    //   const apiUrl=`http://localhost:8000/api/v1/auth/jwt-decode/${value}`;
-    //     const response=await fetch(apiUrl, {method: 'GET'});
-    //     const data=await response.json()
-    //     console.log(data)
-    // }
+  const [keyboardOpen, setKeyboardOpen] = useState(false);
+  const [calandermodalOpen, setCalanderModalOpen] = useState(false);
+  const [task, setTask] = useState<taskMetaData>();
 
   useEffect(() => {
     // const keyboardDidShowListener = Keyboard.addListener(
@@ -54,6 +57,8 @@ const Home = () => {
     //     console.log('keyboard closed');
     //   }
     // );
+    fetchThisWeekTasks();
+
 
     // return () => {
     //   keyboardDidShowListener.remove();
@@ -69,23 +74,39 @@ const Home = () => {
   token_verification();
   }, []);
 
+  // api call for today's task
+  const fetchThisWeekTasks = async () => {
+    try {
+      const userId = 1
+      const res = await axios.get(`${process.env.EXPO_PUBLIC_BACKEND_URL}/api/v1/home/this-week-task/${userId}`)
+      // console.log(res.data.data[0]);
+      if (res.data.success) {
+        setTask(res.data.data[0])
+      } else {
+        console.log(res.data.message);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <SafeAreaView>
-      <ScrollView style={{backgroundColor: '#ecedea'}}
-      automaticallyAdjustContentInsets={false}
-      automaticallyAdjustKeyboardInsets={false}
+      <ScrollView style={{ backgroundColor: '#ecedea' }}
+        automaticallyAdjustContentInsets={false}
+        automaticallyAdjustKeyboardInsets={false}
       >
-        <View style={{justifyContent: 'center', alignItems: 'center', marginTop: keyboardOpen? -200:  10}}>
+        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: keyboardOpen ? -200 : 10 }}>
           <LinearGradient
-          colors={['#377C8B', '#0D3F4A', '#0D3F4A' ]}
-          locations={[0.0, 0.2, 0.6]}
-          style={[Styles.container]}
+            colors={['#377C8B', '#0D3F4A', '#0D3F4A']}
+            locations={[0.0, 0.2, 0.6]}
+            style={[Styles.container]}
           >
-          <View style={[Styles.profile_container]}>
-            <View style={Styles.curb_logo}>
-              <Logo/>
-            </View>
-            <View style={{
+            <View style={[Styles.profile_container]}>
+              <View style={Styles.curb_logo}>
+                <Logo />
+              </View>
+              <View style={{
                 flex: 1,
                 justifyContent: 'flex-end',
                 flexDirection: 'row',
@@ -93,42 +114,45 @@ const Home = () => {
                 width: '100%',
                 marginTop: 20,
                 gap: 10
-            }}>
-                <TouchableOpacity onPress={()=>setCalanderModalOpen(true)}>
-                  <CalendarIcon/>
+              }}>
+                <TouchableOpacity onPress={() => setCalanderModalOpen(true)}>
+                  <CalendarIcon />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>router.push('/myProfileScreen/MyProfile')}>
+                <TouchableOpacity onPress={() => router.push('/myProfileScreen/MyProfile')}>
                   <View style={Styles.home_profile}>
                     <Text style={Styles.home_profile_text}>CJ</Text>
                   </View>
                 </TouchableOpacity>
-            </View>
-          </View>
-          <View style={Styles.drinkFreeDays}>
-            <DrinkFreeDays/>
-          </View>
-          <View>
-            <CaloriesAvoidedWidget/>
-          </View>
-          <View>
-            <View style={{paddingHorizontal: 15}}>
-              <Text style={[styles.task_text, {backgroundColor: 'transparent'}]}>Today's task</Text>
-              <View>
-                {dummyValues.map((item, index)=>
-                      <TaskCard 
-                          key={index}
-                          title={item.title}
-                          description={item.Description}
-                          imageUri={item.imageUri}
-                          Width={0.83}
-                          focus='home'
-                      />
-                )}
               </View>
             </View>
-          </View>
-          {keyboardOpen && (
-          <View
+            <View style={Styles.drinkFreeDays}>
+              <DrinkFreeDays />
+            </View>
+            <View>
+              <CaloriesAvoidedWidget />
+            </View>
+            <View>
+              <View style={{ paddingHorizontal: 15 }}>
+                <Text style={[styles.task_text, { backgroundColor: 'transparent' }]}>Today's task</Text>
+                <View>
+                  {/* {dummyValues.map((item, index)=> */}
+                  {
+                    task &&
+                    <TaskCard
+                      // key={index}
+                      title={task?.attributes.taskTitle}
+                      description={task.attributes.taskDescription}
+                      imageUri={require('../../assets/images/BasketBall.webp')}
+                      Width={0.83}
+                      focus='home'
+                    />
+                  }
+                  {/* )} */}
+                </View>
+              </View>
+            </View>
+            {keyboardOpen && (
+              <View
                 style={{
                   position: 'absolute',
                   height: '100%',
@@ -141,21 +165,18 @@ const Home = () => {
                 }}
               />
             )}
-          </LinearGradient>   
+          </LinearGradient>
         </View>
-        
-        <MotivationWidget/>
-        {/* <Pressable onPress={handleSecret}>
-          <Text>secret</Text>
-        </Pressable> */}
+
+        <MotivationWidget />
         <Modal visible={calandermodalOpen} transparent>
-            <CalenderModal calandermodalOpen={calandermodalOpen} setCalanderModalOpen={setCalanderModalOpen}/>
+          <CalenderModal calandermodalOpen={calandermodalOpen} setCalanderModalOpen={setCalanderModalOpen} />
         </Modal>
-        <View style={{height: windowHeight*0.15}}></View>
+        <View style={{ height: windowHeight * 0.15 }}></View>
       </ScrollView>
 
     </SafeAreaView>
-    
+
   )
 }
 
