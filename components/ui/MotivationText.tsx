a9b92b6f6aaf338b8d2f1c952f06640df71e5d49
smmesbah <mesbah@recruit.so>
2023-12-25 16:43:31 +0600

    Integrate your why cards APIs

--- components/ui/MotivationText.tsx
+++ components/ui/MotivationText.tsx
@@ -1,90 +1,114 @@
-import React from 'react'
-import {Text, StyleSheet, View, TextInput, Dimensions, TouchableWithoutFeedback, Keyboard, TouchableOpacity} from 'react-native'
+import React, { useEffect } from 'react'
+import { Text, StyleSheet, View, TextInput, Dimensions, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
 import { EditIcon } from 'components/icons/EditIcon';
 import { SubmitIcon } from 'components/icons/SubmitIcon';
 import CancelIcon from 'components/icons/CancelIcon';
+import { YourWhyAnswers } from 'components/MotivationWidget';
+import axios from 'axios';
 
-const windowHeight=Dimensions.get('screen').height;
-const windowWidth=Dimensions.get('screen').width;
+const windowHeight = Dimensions.get('screen').height;
+const windowWidth = Dimensions.get('screen').width;
 
 interface MotivationTextProps {
     Data: any,
     id: number,
-    itemtext: string,
+    answers: YourWhyAnswers[],
     onChangeText: any
 }
 
-const MotivationText: React.FC<MotivationTextProps> = ({Data, id, itemtext, onChangeText}) => {
-    const [text, EditText]=React.useState('');
-    const [editOpen, setEditOpen]=React.useState(false);
+const MotivationText: React.FC<MotivationTextProps> = ({ Data, id, answers, onChangeText }) => {
+    // const [text, EditText] = React.useState('');
+    const [editOpen, setEditOpen] = React.useState(false);
+    const [answer, setAnswer] = React.useState<string>('');
 
-    const handleOnChangeText = (text: string) => {
-        onChangeText(text)
-        EditText(text);
+    useEffect(() => {
+        filterAnswers();
+    }, [])
+
+    // filter answers
+    const filterAnswers = () => {
+        const ans = answers.filter((item: YourWhyAnswers) => item.strapiId == id);
+        if (ans[0]) {
+            // console.log(ans[0]);
+            setAnswer(ans[0].answer)
+        } else {
+            // console.log("Not answered")
+            setAnswer('')
+        }
     }
-
-    const handleSubmit=()=>{
-        // if(editOpen){
+    const handleSubmit = async () => {
+        try {
             setEditOpen(!editOpen);
-            Data.map((item: any) => {
-                if(item.id===id){
-                    item.Answer=text;
-                }
+            const res = await axios.post(`${process.env.EXPO_PUBLIC_BACKEND_URL}/api/v1/home/answer-why`, {
+                userId: 1,
+                strapiId: id,
+                answer: answer,
             })
-            console.log(Data);
-        // }
+            if (res.data.success) {
+                console.log(res.data.message);
+                console.log(res.data.data);
+            } else {
+                console.log(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    const answer = Data.filter((item: any) => item.id===id)[0].Answer;
-    const question = Data.filter((item: any) => item.id===id)[0].Question;
-    console.log('answer', answer);
 
-  return (
-    // <TouchableWithoutFeedback onPress={handleTouchOutside}> '#0D3F4A'
-    <LinearGradient
-    colors={['#0D3F4A','#0D3F4A', '#377C8B']}
-    start={{ x: 0, y: 0.5 }}
-    end={{ x: 1, y: 0.5 }}
-    // locations={[0.5, 0.2, 0.0]}
-    style={[styles.input_portion, {marginLeft: id===1? windowWidth*0.09: 0, marginRight: id===Data.length? windowWidth*0.09: 0}]}
-    >
-    {/* <View 
-    style={styles.input_portion}
-    > */}
-        <Text
-            style={styles.text_motivation}
+    const handleCancelPress = () => {
+        setEditOpen(!editOpen);
+        setAnswer('');
+
+    }
+
+    // const answer = Data.filter((item: any) => item.id === id)[0].Answer;
+    const question = Data.filter((item: any) => item.id === id)[0].attributes.Question;
+    // console.log('answer', answer);
+
+    return (
+        <LinearGradient
+            colors={['#0D3F4A', '#0D3F4A', '#377C8B']}
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+            // locations={[0.5, 0.2, 0.0]}
+            style={[styles.input_portion, { marginLeft: id === 1 ? windowWidth * 0.09 : 0, marginRight: id === Data.length ? windowWidth * 0.09 : 0 }]}
         >
-            {question}
-        </Text>
+            <Text style={styles.text_motivation}>
+                {question}
+            </Text>
 
-        {!editOpen && answer!==''? 
-        <Text style={styles.text_input}>{answer}</Text> 
-        :
-        <TextInput
-        onFocus={() => setEditOpen(true) }   //focus received
-        onBlur={() => setEditOpen(false) }
-        multiline={true}
-        style={[styles.text_input,{color: '#ccc'}]}
-        onChangeText={text=>{handleOnChangeText(text)}}
-        value={itemtext}
-        placeholder={answer===''?'Write your Answer?': answer}
-        placeholderTextColor={'#5f6f7b'}
-        textAlignVertical='top'
-      />}
-      <View style={styles.edit_swipe_style}>
-      <Text style={styles.swipe}>* Swipe left to see more goals</Text>
-      {editOpen || answer===''?
-        <View style={{flexDirection: 'row', gap: 7}}>
-            {text!=='' && <TouchableOpacity onPress={()=>setEditOpen(!editOpen)}><CancelIcon/></TouchableOpacity>}
-            <TouchableOpacity onPress={()=>handleSubmit()}><SubmitIcon/></TouchableOpacity>
-        </View> :
-        <TouchableOpacity onPress={()=>setEditOpen(!editOpen)}><EditIcon/></TouchableOpacity>
-      }
-      </View>
-    {/* </View> */}
-    </LinearGradient>
-  )
+            {/* {
+                !editOpen && answer !== '' ?
+                    <Text style={styles.text_input}>{answer}</Text>
+                    : */}
+            <TextInput
+                onFocus={() => setEditOpen(true)}   //focus received
+                // onBlur={() => setEditOpen(false)}
+                multiline={true}
+                style={[styles.text_input, { color: '#ccc' }]}
+                onChangeText={(value) => setAnswer(value)}
+                value={answer}
+                placeholder='Write your Answer?'
+                placeholderTextColor={'#5f6f7b'}
+                textAlignVertical='top'
+            />
+            {/* } */}
+            <View style={styles.edit_swipe_style}>
+                <Text style={styles.swipe}>* Swipe left to see more goals</Text>
+                {
+                    editOpen ?
+                        <View style={{ flexDirection: 'row', gap: 7 }}>
+                            {answer !== '' && <TouchableOpacity onPress={() => handleCancelPress()}><CancelIcon /></TouchableOpacity>}
+                            <TouchableOpacity onPress={() => handleSubmit()}><SubmitIcon /></TouchableOpacity>
+                        </View>
+                        :
+                        <TouchableOpacity onPress={() => setEditOpen(!editOpen)}><EditIcon /></TouchableOpacity>
+                }
+            </View>
+        </LinearGradient>
+    )
 }
 
 export default MotivationText
@@ -94,7 +118,7 @@ const styles = StyleSheet.create({
         fontSize: 12,
         fontFamily: 'Regular',
         letterSpacing: 0.12,
-        lineHeight: 26.25,
+        // lineHeight: 26.25,
         color: '#fff',
         height: 'auto',
         width: '100%',
@@ -107,14 +131,14 @@ const styles = StyleSheet.create({
         letterSpacing: 1,
         lineHeight: 25,
         color: '#fff',
-        height: windowHeight*0.07,
+        height: windowHeight * 0.07,
         width: '100%',
         textAlignVertical: 'top',
         marginTop: 5,
 
     },
     input_portion: {
-        width: windowWidth*.84,
+        width: windowWidth * .84,
         height: 'auto',
         backgroundColor: '#7844ff',
         borderRadius: 15,
@@ -124,9 +148,9 @@ const styles = StyleSheet.create({
         alignItems: 'flex-start',
         // padding: windowWidth*.055,
         // marginHorizontal: 10
-    
+
     },
-    edit_swipe_style:{
+    edit_swipe_style: {
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-between',
