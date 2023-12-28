0b0705458ea1e79b8d103a10ed8c3725a4d335e1
Samina Tasneem <saminatasneem@Saminas-MacBook-Air.local>
2023-12-28 19:41:40 +0600

    Add privacy policy , terms-condition and research policy

---
+++ app/privacy-policy/data/data.tsx
@@ -0,0 +1,177 @@
+export const privacyPolicy1 = [
+  "Getting your feedback and reviews",
+  "Providing our Platform, and ensuring technical bugs are identified and resolved",
+  "Gathering information and developing insights about how users use Curb, including aggregating individuals' data",
+  "Developing and improving Curb",
+  "Customizing Curb for our users",
+  "Implementing and improving our security measures",
+  "Growing our business and informing our marketing strategy",
+  "Marketing and promoting Curb to an organization you work for or provide services to",
+  "Measuring or understanding the effectiveness of advertising we serve to you and others and delivering relevant advertising to you (including when you visit other websites)",
+];
+export const disclosurePoints = [
+  "Our service providers, organizations who support the services we offer through the Platform and only process your personal information on our behalf, following our instructions and data protection law.",
+  "Service providers help us with things like website and data hosting, distributing communications, supporting or updating marketing lists, customer service, facilitating feedback on our services, digital invoicing and payment processing providers, and IT support services.",
+  "These organizations (which may be third-party suppliers, agents, sub-contractors, and/or other companies in our group) will only use your information to the extent necessary to perform their support functions.",
+  "We may provide aggregated and anonymized data to academic institutions, service delivery partners, or research bodies. If we wish to provide any personal information which is not anonymized, we will provide detailed privacy information at the time (including individual opt-ins where applicable). Personally identifiable user data will not be shared outside of Curb without explicit user permission.",
+  "If we run surveys, competitions, promotional campaigns, offers, or other occasional activities and you opt-in with a partner (for example, if you chose to enter a prize draw we manage with a partner organization). We will provide more detailed privacy information at the time.",
+  "Our auditors, legal advisers, and other professional advisers.",
+  "Potential investors, or if we sell or buy any business or assets then a potential seller or buyer.",
+  "Any person to whom disclosure is necessary for us to protect our rights, property, or safety, our clients, or other third parties, and to enforce our rights under this Notice or under any agreement (for example, our Terms of Service) with you. This includes exchanging information with other companies and organizations for the purposes of detecting and preventing fraud and cyber-crime.",
+  "If required to do so by court order or if we are under a duty to disclose your information in order to comply with (and/or where we believe we are under a duty to comply with) any legal obligation. This includes exchanging information with law enforcement agencies, regulators, or other similar government bodies.",
+];
+export const dataUsagePurposes = [
+  "To store it.",
+  "So we can provide our Platform and Services to you.",
+  "To support the operation of our business, where this is in our legitimate interests and we have concluded these are not overridden by your rights.",
+  "Where we are legally required to do so.",
+];
+
+export const dataProcessingEntities = [
+  {
+    name: "Amazon Web Services, Inc.",
+    location: "United Kingdom",
+    clauses: "Standard Contractual Clauses",
+  },
+  {
+    name: "Google LLC (Google Cloud, Google Ads / Analytics)",
+    location: "United States",
+    clauses: "Standard Contractual Clauses",
+  },
+  {
+    name: "Google Ads",
+    location: "United States",
+    clauses: "Standard Contractual Clauses",
+  },
+  {
+    name: "Segment.io, Inc",
+    location: "United States",
+    clauses: "Standard Contractual Clauses",
+  },
+  {
+    name: "Mixpanel, Inc.",
+    location: "United States",
+    clauses: "Standard Contractual Clauses",
+  },
+];
+
+export const retentionInfo = [
+  "For as long as necessary for the original reasons we collected it (for example, for as long as you have an account with us)",
+  "For up to six years after that to identify any issues and resolve any legal proceedings.",
+];
+
+export const disclosureInfo = [
+  "In the event of a complaint,",
+  "If we reasonably believe there is a prospect of legal proceedings,",
+  "If we are aware of pending or ongoing legal proceedings, or",
+  "In some circumstances, if applicable law says we must.",
+];
+export const dataUsagePurposes2 = [
+  "Better understand how people use Curb",
+  "Develop useful insights and improvements to Curb",
+  "To provide partners or academic organizations with insight into service usage and aggregated outcomes information",
+];
+export const userRights = [
+  "To be provided with a copy of your personal information",
+  "To ask us to correct or delete your personal information",
+  "To request us to restrict how we use your personal information (for example, while we investigate your concerns about the accuracy of data, or lawfulness of a certain use)",
+  "To object to the further use of your personal information, including the right to object to marketing from us",
+  "To request that your provided personal data be moved to a third party",
+  "Where you have consented, to withdraw consent",
+];
+
+export const informationCommissionerOfficePoints = [
+  "The Information Commissioner's Office (ICO) is the supervisory authority in the UK and can provide further information about your rights, an organization's obligations in relation to your personal information, as well as deal with any complaints that you may have.",
+  "You can visit their website at www.ico.org.uk.",
+  "If you are outside the UK, you can find your local data protection authority here.",
+];
+export const functionalityCookies = [
+  "Cookie name",
+  "Why we use it",
+  "Expires",
+  "NONE",
+  "NA",
+  "NA",
+  "Functionality cookies",
+];
+export const cookiesInformationArray = [
+  "Cookie name",
+  "Why we use it",
+  "Expires",
+  "Hotjar",
+  "To understand user experience on the website, in order to improve the designs",
+  "NA",
+  "Webflow.io",
+  "To count website views and understand characteristics of users",
+  "NA",
+  "Google Analytics",
+  "To count website views and understand characteristics of users",
+  "NA",
+  "Targeting/Advertising cookies",
+];
+export const socialNetworkingCookies = [
+  "Cookie name",
+  "Why we use it",
+  "Expires",
+  "Doubleclick.net (Google Ads)",
+  "To support personalization in search results, including adverts",
+  "NA",
+  "Facebook.com",
+  "To support personalization in Facebook feed results, including adverts",
+  "NA",
+  "Pinterest.com",
+  "To support personalization in Pinterest feed results, including adverts",
+  "NA",
+  "LinkedIn.com",
+  "To support personalization in LinkedIn feed results, including adverts",
+  "NA",
+  "Social networking cookies",
+];
+export const socialSharingCookies = [
+  "Cookie name",
+  "Why we use it",
+  "Expires",
+  "Twitter.com",
+  "To enable users to share a Curb article directly to their Twitter feed if they choose",
+  "NA",
+  "Facebook.com",
+  "To enable users to share a Curb article directly to their Facebook feed if they choose",
+  "NA",
+  "LinkedIn.com",
+  "To enable users to share a Curb article directly to their LinkedIn feed if they choose",
+  "NA",
+];
+export const disablingCookiesInfo = [
+  "Cookie name",
+  "Why we use it",
+  "Expires",
+  "NONE",
+  "NA",
+  "NA",
+  "Disabling cookies",
+];
+export const microsoft = [
+  "1. Select the Tools menu > Internet Options",
+  "2. Click on the Privacy tab",
+  "3. Click on Advanced within the Settings section and select the appropriate setting",
+];
+export const google = [
+  "1. Select Settings > Advanced",
+  "2. Under Privacy and Security > Content settings.",
+  "3. Click Cookies and select the relevant options",
+];
+
+export const safari = [
+    "1. Select Preferences > Privacy",
+    "2. Click on Remove all Website Data"
+]
+export const mozilla = [
+    'Choose the Tools menu > Options',
+    'Click on the Privacy icon',
+    'Select the Cookie menu and select the relevant options',
+  ];
+  export const opera=[
+    '1. Choose Files menu > Preferences',
+    '2. Select Privacy',
+  ]
+  
